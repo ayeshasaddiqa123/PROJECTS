161c6db4d1d8a68621f53ad82022576c636b3cec
ayeshasaddiqa123 <ayeshasaddiqawebdev@gmail.com>
2025-07-07 19:19:40 +0500

    till aritmetic operations

--- PROJECT1-CALCULATOR.cpp
+++ PROJECT1-CALCULATOR.cpp
@@ -1,18 +1,56 @@
 #include<iostream>     // include the iostream library (a preprocessor directive)
 using namespace std;     // use the stanadard namespace
 int sum1(int a);
+int differ(int );
+int div (int );
+int mul (int);
+int a;
 int main ()               // main function from where execution starts
 {
-int sum=0;
-int a , c;
+
+int  c;
 char ch;
+int b;
+cin>>b;
 
 while (ch!='\n')
 {
 
-//cout<<"Enter a number to sum : ";
-cin>>a;
-  c=sum1(a);
+   int z=b;
+if (ch == '+')
+
+   {
+   cin>>a;
+  c=z+sum1(a);
+   }
+   
+else if ( ch == '-')
+
+ {
+   cin>>a;
+   int d = differ(a);
+   if ( d < 0)
+   
+  c=z+d;
+  else
+  c= z-d;
+  
+   }
+   
+else if ( ch == '/')
+
+ {
+   cin>>a;
+  c=z/div(a);
+   }   
+   
+else if ( ch == '*')
+
+ {
+   cin>>a;
+  c=z*mul(a);
+   }   
+   
 ch=cin.get(); 
 }
 cout<<c;
@@ -24,4 +62,20 @@ int sum1(int a)
 {
 static	int sum =0;
 return sum=sum+a;	
+}
+int differ(int a)
+{
+	static int diff =0;
+	return diff=diff-a;
+}
+
+int div (int a)
+{
+		static int division =a;
+	return division;
+}
+int mul (int)
+{
+static	int m=1;
+return m=m*a;
 }
